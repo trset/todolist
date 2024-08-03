b8a0c7ee75e5dccfc0d49c6b62395609ea9eb367
trset <himanshu.sha512@gmail.com>
2024-08-03 17:42:08 +0530

    add icons

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,27 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('matrix-todo-cache').then(cache => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './style.css',
-          './script.js',
-          './manifest.json',
-          './icons/icon-192x192.png',
-          './icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('matrix-todo-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icons/android-chrome-192x192.png',
+        './icons/android-chrome-512x512.png',
+        './icons/apple-touch-icon.png',
+        './icons/favicon-16x16.png',
+        './icons/favicon-32x32.png',
+        './icons/favicon.ico'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
