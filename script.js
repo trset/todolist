d5b95375793d3454f97dda2bb99c142a13524016
trset <himanshu.sha512@gmail.com>
2024-08-03 17:29:00 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
 function getTodosLocalStorage(){
     const todosLocalStr = localStorage.getItem("todolist");
     if(todosLocalStr != null && todosLocalStr != "") {
